ff62c99153380c870eccb20244e27db814aee9e8
feige996 <1020102647@qq.com>
2026-02-03 14:35:32 +0800

    fix(cli): 修复 removeGitFolder 函数未定义问题

--- packages/cli/src/utils/cloneRepo.ts
+++ packages/cli/src/utils/cloneRepo.ts
@@ -1,5 +1,6 @@
 import type { PromptResult } from '../types'
 import { exec } from 'node:child_process'
+import { promises as fsPromises } from 'node:fs'
 import { join } from 'node:path'
 import process from 'node:process'
 import { log } from '@clack/prompts'
@@ -7,6 +8,11 @@ import { red } from 'kolorist'
 import { replacePackageJson } from './replacePackageJson'
 
 const REPO_URL = 'https://gitee.com/feige996/unibest.git'
+
+async function removeGitFolder(localPath: string): Promise<void> {
+  const gitFolderPath = join(localPath, '.git')
+  await fsPromises.rm(gitFolderPath, { recursive: true, force: true })
+}
 
 async function cloneRepo(projectName: string, branch: string): Promise<void> {
   log.info('从 Git 克隆基础模板...')
